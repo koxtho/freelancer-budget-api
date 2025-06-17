eda7e327cab71c95f5890820be9ba89996bdaef4
koxtho <coxkevin097@gmail.com>
2025-06-17 15:18:50 -0700

    Initial commit - express server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('starting the Server.....');
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
